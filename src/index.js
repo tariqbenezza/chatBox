afcf270c8f91e7f77773a9c6acebefde816bd0c5
tariqbenezza <tariq.benezza@gmail.com>
2017-08-23 17:37:36 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//REACT
+import React from 'react';
+import {render} from 'react-dom';
+//Component
+import Connexion from './components/Connexion'
+import App from './components/App'
+//CSS
+import './index.css';
+
+render(
+	<App />,
+	document.getElementById('root')
+);
