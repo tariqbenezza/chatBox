a4fe67eaa989f87ad1dd0b2481227f29b4444051
tariqbenezza <tariq.benezza@gmail.com>
2017-08-25 17:32:00 +0200

    Firebase & CSS

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Formulaire from './Formulaire';
 import Message from './Message';
+import base from '../base';
+// CSS
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import '../animation.css'
 
 class App extends React.Component {
 
 	state = {
 		messages : {}
+	}
 
+	componentWillMount() {
+		this.ref = base.syncState('/',{
+			context: this,
+			state: 'messages'
+		});
+	}
+
+	componentDidUpdate() {
+		//Block Scroll
+		this.messages.scrollTop = this.messages.scrollHeight; 
 	}
 
 	addMessage = message => {
@@ -15,22 +30,38 @@ class App extends React.Component {
 		//Ajouter le timestamp
 		const timestamp = Date.now();
 		messages[`message-${timestamp}`] = message;
+		//delete if more than 10 messages
+		Object.keys(messages).slice(0, -10).map(
+			key => messages[key] = null
+		);
 		//State update
 		this.setState({messages});
 	};
+
+	isUser = (pseudo) => {
+		return pseudo === this.props.params.pseudo;
+	}
 
 	render() {
 
 		const messages = Object
 			.keys(this.state.messages)
-			.map(key => <Message key={key} details={this.state.messages[key]}/>)
+			.map(key => <Message key={key} details={this.state.messages[key]} isUser={this.isUser}/>)
 		;
 
 		return (
 			<div className="box">
 				<div>
-					<div className="messages">
-						{messages}
+					<div className="messages" ref={input => this.messages = input}>
+						<ReactCSSTransitionGroup
+							component="div"
+							className="message"
+							transitionName="message"
+							transitionEnterTimeout={200}
+							transitionLeaveTimeout={200}
+						>
+							{messages}
+						</ReactCSSTransitionGroup>
 					</div>
 					<Formulaire 
 						addMessage={this.addMessage} 
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class Message extends React.Component {
+
+	preRender = (isUser) => {
+		if(isUser){
+			return(
+				<p className="user-message">
+					{this.props.details.message}
+				</p>
+			)
+		}
+	};
+
 	render() {
 		return (
 			<p className="user-message">
