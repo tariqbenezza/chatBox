23dcae214c2e1c5a2306767fea566cfe0163bef5
tariqbenezza <tariq.benezza@gmail.com>
2017-08-28 17:21:51 +0200

    firebase setup

--- src/base.js
+++ src/base.js
@@ -1,9 +1,9 @@
 import Rebase from 're-base';
 
 const base = Rebase.createClass({
-	apiKey: "**********************",
-	authDomain: "***********************",
-	databaseURL: "****************************"
+	apiKey: "AIzaSyAvhpz-NoT_5Fo9nAiOknBDM3cArZ_-BGU",
+	authDomain: "ma-chat-box-d870c.firebaseapp.com",
+	databaseURL: "https://ma-chat-box-d870c.firebaseio.com"
 });
 
 export default base;
