3fc34a0c08cd04da8591a9a6d9efd6cb62934de3
Codeship Build <build@codeship.com>
2017-08-28 15:29:38 +0000

    Updates

--- src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-	apiKey: "**********************",
-	authDomain: "***********************",
-	databaseURL: "****************************"
-});
-
-export default base;
